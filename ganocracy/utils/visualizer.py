62ec0f4f299d33c5943969d59c2ab23c87c1de5b
Alex Andonian <alexandonian@gmail.com>
2019-05-18 00:00:08 +0000

    Checkpoint work on nb, just in case...

--- ganocracy/utils/visualizer.py
+++ ganocracy/utils/visualizer.py
@@ -1,13 +1,41 @@
+import threading
+
 import torch
 import numpy as np
+
+
 import torchvision.utils as vutils
 import matplotlib.pyplot as plt
 
 
-def visualize_dataset(dataset, num_samples=64, figsize=(15, 15), title='Real Images'):
-    data = torch.stack([dataset[i][0] for i in range(num_samples)])
+def visualize_data(data, num_samples=64, figsize=(15, 15), title='Real Images'):
+    if isinstance(data, torch.utils.data.Dataset):
+        print(data)
+        samples = torch.stack([data[i][0] for i in range(num_samples)])
+    elif isinstance(data, torch.utils.data.DataLoader):
+        print(data.dataset)
+        samples = next(iter(data))[0][:num_samples]
+    else:
+        raise ValueError(f'Unrecognized data source type: {type(data)}'
+                         'Must be instance of either torch Dataset or DataLoader')
     # Plot the real images
     plt.figure(figsize=figsize)
     plt.axis("off")
     plt.title(title)
-    plt.imshow(np.transpose(vutils.make_grid(data, padding=5, normalize=True).cpu(), (1, 2, 0)))
+    plt.imshow(np.transpose(vutils.make_grid(samples, padding=5, normalize=True).cpu(), (1, 2, 0)))
+
+
+def _save_sample(G, fixed_noise, filename, nrow=8, padding=2, normalize=True):
+    fake_image = G(fixed_noise).detach()
+    vutils.save_image(fake_image, filename, nrow=nrow, padding=padding, normalize=normalize)
+
+
+def save_samples(G, fixed_noise, filename, threaded=True):
+    if threaded:
+        G.to('cpu')
+        thread = threading.Thread(name='save_samples',
+                                  target=_save_sample,
+                                  args=(G, fixed_noise, filename))
+        thread.start()
+    else:
+        _save_sample(G, fixed_noise, filename)
