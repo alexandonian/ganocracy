2f52b20c977915994470d038e4e274ceaf21ee4d
Alex Andonian <alexandonian@gmail.com>
2019-05-25 08:54:59 +0000

    Some updates

--- ganocracy/layers/__init__.py
+++ ganocracy/layers/__init__.py
@@ -1,4 +1,6 @@
 
 from .norm import SNConv2d, SNLinear, SNEmbedding, ConditionalBatchNorm2d
+from .biggan_layers import GBlock, DBlock, bn, ccbn, Attention
 
-__all__ = ['SNConv2d', 'SNLinear', 'SNEmbedding', 'ConditionalBatchNorm2d']
+__all__ = ['SNConv2d', 'SNLinear', 'SNEmbedding', 'ConditionalBatchNorm2d',
+           'bn', 'ccbn', 'GBlock', 'DBlock', 'Attention']
